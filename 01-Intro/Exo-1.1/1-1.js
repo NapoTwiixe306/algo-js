f421f346431d8db5fecdd246e1e633eecd93b3dc
napotwiixe <julien.milants@gmail.com>
2023-06-08 09:32:57 +0200

    first commiut

---
+++ 01-Intro/Exo-1.1/1-1.js
@@ -0,0 +1,3 @@
+let name = "Julien";
+
+console.log(name)
---
+++ 01-Intro/Exo-1.2/1-2.js
@@ -0,0 +1,5 @@
+let name = "Julien ";
+let firstName = "Milants";
+let city = "Liers";
+
+console.log("Your name is " + name + firstName + " you live in " + city)
